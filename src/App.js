89aae89a1da7a511bb7e9f9facca1f45721874ed
maws <maws.madzik@gmail.com>
2024-02-28 22:15:39 +0100

    using sass (with bootstrap scss) instead of css files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './App.scss';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
@@ -20,7 +20,7 @@ const App = () => {
     return (
         <AppProvider>
             <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
+                <h1 className='App-header'>Company's Budget Allocation</h1>
                 <div className='row mt-3'>
                     <div className='col-sm'>
                         <Budget />
